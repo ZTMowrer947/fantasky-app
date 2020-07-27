948dd6b9541155e78a670ae02f40fd15a8aa20df
Zackery Mowrer <Zackmowrer@gmail.com>
2020-07-27 11:24:06 -0400

    Tweak sequelize model setup to be more typesafe

--- src/models/index.js
+++ src/models/index.js
@@ -1,23 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable import/no-dynamic-require */
-/* eslint-disable global-require */
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
 // Imports
-import fs from 'fs';
-import path from 'path';
 import { Sequelize } from 'sequelize';
 
 import config from '../config/config';
 
-const basename = path.basename(__filename);
+// Configuration setup
 const env = process.env.NODE_ENV ?? 'devlelopment';
-const db = {};
 
 const envConfig = config[env];
 
+// Initialize database connection
 /**
  * @type {Sequelize}
  */
@@ -28,27 +21,7 @@ const sequelize = new Sequelize(
   envConfig
 );
 
-fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
-      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
-    );
-  })
-  .forEach((file) => {
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
-    db[model.name] = model;
-  });
+// Model initialization
 
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-export default db;
+// Exports
+export { sequelize, Sequelize };
