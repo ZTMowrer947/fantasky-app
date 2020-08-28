d39fac95e1fe639858216e470f0f7739068f99e1
Zackery Mowrer <Zackmowrer@gmail.com>
2020-08-28 15:05:33 -0400

    Correct closeDatabaseOnError error catching

--- src/middleware/closeDatabaseOnError.js
+++ src/middleware/closeDatabaseOnError.js
@@ -1,17 +1,19 @@
-// Imports
-import asyncHandler from 'express-async-handler';
-
 // Error-handling middleware
 async function closeDatabaseOnError(err, req, res, next) {
-  // If the database is connected,
-  if (req.db?.isConnected) {
-    // Close the database connection
-    await req.db.close();
-  }
+  try {
+    // If the database is connected,
+    if (req.db?.isConnected) {
+      // Close the database connection
+      await req.db.close();
+    }
 
-  // Pass error next handler
-  next(err);
+    // Pass error next handler
+    next(err);
+  } catch (error) {
+    // If any error occurs during this process, pass that to error handlers
+    next(error);
+  }
 }
 
 // Exports
-export default asyncHandler(closeDatabaseOnError);
+export default closeDatabaseOnError;
