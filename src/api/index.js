d2ba92ef895fa92cd4a6e86a1eaca055d7fee2b3
Zackery Mowrer <Zackmowrer@gmail.com>
2020-08-28 15:09:15 -0400

    Add API error handler

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,8 @@
 // Imports
 import express from 'express';
+import createError from 'http-errors';
+
+import apiErrorHandler from './middleware/apiErrorHandler';
 
 // Express sub-app setup
 const api = express();
@@ -9,6 +12,19 @@ api.disable('x-powered-by');
 
 // Middleware
 api.use(express.json());
+
+// Routes
+api.all('*', async (req, res, next) => {
+  // Create 404 error
+  const message = `Cannot ${req.method} ${req.path}`;
+  const error = createError(404, message);
+
+  // Pass to error handlers
+  next(error);
+});
+
+// Error handlers
+api.use(apiErrorHandler);
 
 // Exports
 export default api;
