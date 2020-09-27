ff37e776abb8b288b4989916ace40be950796c47
Zackery Mowrer <Zackmowrer@gmail.com>
2020-09-27 18:19:24 -0400

    Setup "basic" passport strategy

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,14 @@
 // Imports
 import express from 'express';
 import createError from 'http-errors';
+import passport from 'passport';
+import { BasicStrategy } from 'passport-http';
 
 import apiErrorHandler from './middleware/apiErrorHandler';
 import userRoutes from './routes/users';
+import bootstrapDatabase from '../bootstrapDatabase';
 import closeDatabaseOnError from '../middleware/closeDatabaseOnError';
+import UserService from '../services/UserService';
 
 // Express sub-app setup
 const api = express();
@@ -14,6 +18,50 @@ api.disable('x-powered-by');
 
 // Middleware
 api.use(express.json());
+api.use(passport.initialize());
+
+// Authentication strategies
+passport.use(
+  new BasicStrategy(async (emailAddress, password, done) => {
+    // Create database connection
+    const connection = await bootstrapDatabase();
+
+    try {
+      // Instantiate user service
+      const service = new UserService(connection);
+
+      // Use service to verify credentials
+      const credentialsValid = await service.verifyCredentials(
+        emailAddress,
+        password
+      );
+
+      // If they are invalid,
+      if (!credentialsValid) {
+        // Close database connection
+        await connection.close();
+
+        // Deny access
+        return done(null, false);
+      }
+
+      // Otherwise, retrieve user data
+      const user = await service.getByEmail(emailAddress);
+
+      // Close database connection
+      await connection.close();
+
+      // Grant access
+      return done(null, user);
+    } catch (error) {
+      // If an error occurs, close database connection
+      await connection.close();
+
+      // Pass error through callback
+      return done(error);
+    }
+  })
+);
 
 // Routes
 api.use('/users', userRoutes);
