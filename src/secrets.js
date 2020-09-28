0eb7ea972a2897010c4012fd1a5039b145cc5a7d
Zackery Mowrer <Zackmowrer@gmail.com>
2020-09-28 16:14:26 -0400

    Reorganize secrets file to use named exports
    Remove unneeded TLS secrets

--- src/secrets.js
+++ src/secrets.js
@@ -4,40 +4,11 @@
  * Contains the secret values used within the application that must not be directly exposed to the user.
  */
 
-// Imports
-import fs from 'fs';
+// Secret for JSON Web Tokens
+export const jwtSecret = process.env.JWT_SECRET;
 
-// TLS secrets
+// Secret for cookie signing
+export const cookieSecret = process.env.COOKIE_SECRET;
 
-// Whether to use TLS for securing connections
-const useTls = process.env.USE_TLS;
-
-// TLS client certificate
-const cert = process.env.TLS_CERT
-  ? fs.readFileSync(process.env.TLS_CERT)
-  : undefined;
-
-// TLS client private key
-const key = process.env.TLS_KEY
-  ? fs.readFileSync(process.env.TLS_KEY)
-  : undefined;
-
-// TLS certificate authority for validating server certificate
-const ca = process.env.TLS_CA ? fs.readFileSync(process.env.TLS_CA) : undefined;
-
-// Diffie-Hellman parameters to enable forward secrecy
-const dhparams = process.env.TLS_DHPARAMS
-  ? fs.readFileSync(process.env.TLS_DHPARAMS)
-  : undefined;
-
-// Secrets object
-const secrets = {
-  useTls,
-  ca,
-  cert,
-  key,
-  dhparams,
-};
-
-// Exports
-export default secrets;
+// Secret for session data
+export const sessionSecret = process.env.SESSION_SECRET;
