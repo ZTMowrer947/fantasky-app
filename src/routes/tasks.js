3456656677f43082dab9c35a3aa7d21ca9fb2559
Zackery Mowrer <Zackmowrer@gmail.com>
2020-10-09 19:05:38 -0400

    Modify GET tasks to retrieve tasks of user

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,6 +1,10 @@
 // Imports
 import { ensureLoggedIn } from 'connect-ensure-login';
 import { Router } from 'express';
+import asyncHandler from 'express-async-handler';
+
+import database from '../middleware/database';
+import TaskService from '../services/TaskService';
 
 // Express router setup
 const taskRoutes = Router();
@@ -8,10 +12,25 @@ const taskRoutes = Router();
 // Routes
 taskRoutes
   .route('/') // /tasks
-  .get(ensureLoggedIn('/login'), (req, res) => {
-    // TODO: Load tasks of logged in user
-    res.render('tasks/index');
-  });
+  .get(
+    ensureLoggedIn('/login'),
+    database,
+    asyncHandler(async (req, res) => {
+      // Instantiate task service
+      const service = new TaskService(req.db);
+
+      // Retrieve tasks for user
+      const tasks = await service.findAllForUser(req.user.id);
+
+      // Close database connection
+      await req.db.close();
+
+      console.log(tasks);
+
+      // TODO: Process and attach tasks of logged in user
+      res.render('tasks/index');
+    })
+  );
 
 // Exports
 export default taskRoutes;
