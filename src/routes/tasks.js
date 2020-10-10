13f1ceb84bd85f749c8849c16a4d054dabca44b9
Zackery Mowrer <Zackmowrer@gmail.com>
2020-10-10 05:17:28 -0400

    Complete task listing feature

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -21,16 +21,127 @@ taskRoutes
     ensureLoggedIn('/login'),
     database,
     asyncHandler(async (req, res) => {
-      // // Instantiate task service
-      // const service = new TaskService(req.db);
+      // Instantiate task service
+      const service = new TaskService(req.db);
 
-      // // Retrieve tasks for user
-      // const tasks = await service.findAllForUser(req.user.id);
+      // Retrieve tasks for user
+      const tasks = await service.findAllForUser(req.user.id);
 
       // Close database connection
       await req.db.close();
 
-      // TODO: Process and attach tasks of logged in user
+      const daysOfWeek = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+      ];
+      const weekDays = daysOfWeek.slice(1, 6);
+      const weekends = [daysOfWeek[0], daysOfWeek[6]];
+
+      // Map tasks into view model data
+      const taskViewData = tasks.map((task) => {
+        // Convert binary day representation into boolean values
+        const [activeDays] = Array.from({ length: 7 }).reduce(
+          ([daysActive, daysBinary]) => {
+            const dayIsActive = daysBinary % 2 !== 0;
+            const quotient = Math.trunc(daysBinary / 2);
+
+            return [[dayIsActive, ...daysActive], quotient];
+          },
+          [[], task.daysToRepeat]
+        );
+
+        // Convert boolean values into string representations
+        const activeDayStrings = activeDays
+          .map((dayActive, index) => {
+            return dayActive ? daysOfWeek[index] : dayActive;
+          })
+          .filter((day) => day);
+
+        // Join days together
+        let activeDayString = 'Every '.concat(activeDayStrings.join(', '));
+
+        // Replace with special text if days are the whole week, the weekdays, or the weekends
+        if (activeDayStrings.length === activeDays.length) {
+          activeDayString = 'Every day';
+        } else if (
+          activeDayStrings.length === 5 &&
+          activeDayString === weekDays.join(', ')
+        ) {
+          activeDayString = 'Every weekday';
+        } else if (
+          activeDayStrings.length === 2 &&
+          activeDayString === weekends.join(', ')
+        ) {
+          activeDayString = 'Every weekend';
+        } else if (activeDayStrings.length > 3) {
+          const truncatedActiveDayStrings = activeDayStrings.map((day) =>
+            day.substring(0, 3)
+          );
+
+          activeDayString = 'Every '.concat(
+            truncatedActiveDayStrings.join(', ')
+          );
+        }
+
+        const streak = [];
+
+        for (let i = task.completedDays.length - 1; i >= 0; i -= 1) {
+          // Get day being processed
+          const day = task.completedDays[i];
+
+          // Parse date as ISO date string
+          const parsedDate = DateTime.fromISO(day.date, { zone: 'utc' });
+
+          // If this is the last element in the array,
+          if (i === task.completedDays.length - 1) {
+            // Prepend to streak
+            streak.unshift(parsedDate);
+          } else {
+            // Otherwise, determine date of next day
+            const dayAfterParsedDate = parsedDate.plus({ days: 1 });
+
+            // Get day of element immediately following this one
+            const nextDay = task.completedDays[i + 1];
+
+            // Parse date of that day
+            const nextDate = DateTime.fromISO(nextDay.date, { zone: 'utc' });
+
+            // If the dates match,
+            if (nextDate.equals(dayAfterParsedDate)) {
+              // Prepend current date to streak
+              streak.unshift(parsedDate);
+            } else {
+              // Otherwise, stop here
+              break;
+            }
+          }
+        }
+
+        // Determine streak text
+        const streakText =
+          streak.length > 0
+            ? `Streak ongoing since ${streak[0].toLocaleString(
+                DateTime.DATE_SHORT
+              )}`
+            : 'No Streak';
+
+        return {
+          id: task.id,
+          name: task.name,
+          activeDays: activeDayString,
+          streak: streakText,
+        };
+      });
+
+      // Attach view data to locals
+      res.locals.tasks = taskViewData;
+
+      // Render task listing for user
       res.render('tasks/index');
     })
   );
