4e85cce626b4a95a1aae579dbc7fa4e181f43848
Zackery Mowrer <Zackmowrer@gmail.com>
2020-10-11 22:04:52 -0400

    Add support for redis auth

--- src/secrets.js
+++ src/secrets.js
@@ -20,4 +20,5 @@ export const sessionSecret = process.env.SESSION_SECRET;
 export const redisOptions = {
   port: Number.parseInt(process.env.REDIS_PORT, 10) ?? 6379,
   host: process.env.REDIS_HOST ?? 'localhost',
+  password: process.env.REDIS_PASS ?? null,
 };
