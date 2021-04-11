dc910253c783a4195f257c78560ff00757507552
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2021-04-10 20:07:27 -0400

    Add styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,24 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { FC } from 'react';
+import { createGlobalStyle } from 'styled-components';
 import { AppProps } from 'next/app';
 import { Provider } from 'next-auth/client';
 
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+`;
+
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <GlobalStyle />
+      {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access */}
+      <Provider session={pageProps.session}>
+        <Component {...pageProps} />
+      </Provider>
+    </>
   );
 };
 
