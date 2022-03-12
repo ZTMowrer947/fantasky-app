4d88443eba95a94a62d44ee81b01587965bcdb4d
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2022-03-12 10:34:44 -0500

    Remove obsolete active day (de)serialization fns

--- src/lib/helpers/days.ts
+++ src/lib/helpers/days.ts
@@ -18,35 +18,7 @@ const shortenedDaysOfWeek = {
   sat: 'Sa',
 };
 
-export function serializeDaysToRepeat(days: DaysToRepeat) {
-  // Extract booleans
-  const { sun, mon, tue, wed, thu, fri, sat } = days;
-
-  // Reverse and store as array
-  const digits = [sat, fri, thu, wed, tue, mon, sun];
-
-  const digitString = digits.map((digit) => Number(digit).toString()).join('');
-
-  return Number.parseInt(digitString, 2);
-}
-
-export function deserializeDaysToRepeat(serialized: number): DaysToRepeat {
-  // Format serialized value as base-2
-  let binaryString = serialized.toString(2);
-
-  // Pad string if needed
-  if (binaryString.length < 7) {
-    binaryString = '0'.repeat(7 - binaryString.length) + binaryString;
-  }
-
-  // Split into digits and convert to booleans
-  const [sat, fri, thu, wed, tue, mon, sun] = binaryString
-    .split('')
-    .map((digit) => !!Number.parseInt(digit, 2));
-
-  return { sun, mon, tue, wed, thu, fri, sat };
-}
-
+// eslint-disable-next-line import/prefer-default-export
 export function formatDaysToRepeat(days: DaysToRepeat) {
   if (Object.values(days).every((isActive) => isActive)) {
     return 'Every day';
