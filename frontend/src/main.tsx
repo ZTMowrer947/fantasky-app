2d81cf7c2d206af08f67e28a4ade8aa30ccabaa8
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2023-11-07 16:23:16 -0600

    Start Supertokens client setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,18 @@
-import { render } from 'preact'
-import { App } from './app.tsx'
-import './index.css'
+import { render } from "preact";
+import SuperTokens from "supertokens-web-js";
+import Session from "supertokens-web-js/recipe/session";
+import EmailPassword from "supertokens-web-js/recipe/emailpassword";
 
-render(<App />, document.getElementById('app')!)
+import { App } from "./app.tsx";
+import "./index.css";
+
+SuperTokens.init({
+  appInfo: {
+    apiDomain: `${window.location.protocol}//${window.location.host}`,
+    apiBasePath: "/api/auth",
+    appName: "Fantasky",
+  },
+  recipeList: [Session.init(), EmailPassword.init()],
+});
+
+render(<App />, document.getElementById("app")!);
