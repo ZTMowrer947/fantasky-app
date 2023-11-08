884ecf756ce3c2e7e72af2d0305f3ee510a02599
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2023-11-08 17:05:50 -0600

    initialize database context for API

--- backend/Mowrer.Fantasky.Api/Program.cs
+++ backend/Mowrer.Fantasky.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Mowrer.Fantasky.Api;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<Context>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("Fantasky")));
 
 var app = builder.Build();
 
@@ -14,6 +19,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    using var scope = app.Services.CreateScope();
+    using var context = scope.ServiceProvider.GetRequiredService<Context>();
+
+    context.Database.Migrate();
 }
 
 app.UseHttpsRedirection();
